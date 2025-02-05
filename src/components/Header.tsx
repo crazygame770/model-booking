3c63eb593dc8fd5af24e3f995b264e90bd1b5cc2
Lucas <lucas20041017@gmail.com>
2025-02-04 23:58:16 -0300

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Header.module.css';
 
 export default function Header() {
@@ -8,7 +9,14 @@ export default function Header() {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Link href="/" className={styles.logo}>
-          ModelBooking
+          <Image
+            src="/logo.jpeg"
+            alt="ModelBooking Logo"
+            width={48}
+            height={48}
+            className={styles.logoImage}
+          />
+          <span>ModelBooking</span>
         </Link>
         <div className={styles.authLinks}>
           <Link href="/model/register" className={styles.link}>
